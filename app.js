d2a8dcc42479ea6f179bd9caacea0ea51386a185
tezzzar <olexandr.borodavko@gmail.com>
2022-09-27 16:36:44 +0300

    add app.js and click

--- app.js
+++ app.js
@@ -1,29 +1,19 @@
-const heartClick = document.querySelector(".prod-pic3");
-const style = getComputedStyle(heartClick);
-var pic = style.backgroundImage;
-
-// console.log(pic);
-
-// var picChange = function () {
-//   heartClick.style.backgroundImage =
-//     'url("http://127.0.0.1:5500/images/heart-white.png")';
-//   heartClick.style.backgroundColor = "#2c71b8";
-// };
+var heartClick = document.querySelector(".prod-pic3");
+var style = getComputedStyle(heartClick);
 
 heartClick.addEventListener("click", function () {
   if (
     heartClick.style.backgroundImage ===
-    'url("http://127.0.0.1:5500/images/heart-blue.png")'
+    'url("http://127.0.0.1:5500/images/heart-white.png")'
   ) {
-    heartClick.style.backgroundImage =
-      'url("http://127.0.0.1:5500/images/heart-white.png")';
-    heartClick.style.backgroundColor = "#2c71b8";
-  } else {
     heartClick.style.backgroundImage =
       'url("http://127.0.0.1:5500/images/heart-blue.png")';
     heartClick.style.backgroundColor = "unset";
+  } else {
+    heartClick.style.backgroundImage =
+      'url("http://127.0.0.1:5500/images/heart-white.png")';
+    heartClick.style.backgroundColor = "#2c71b8";
   }
 });
 
-// picChange();
-console.log(heartClick.style.backgroundImage);
+console.log(heartClick);
